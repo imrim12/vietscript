b4b30965e56e1dbb09929cf26290f10cfea00f92
imrim12 <nguyenhuunguyeny.ny@gmail.com>
2023-03-30 17:38:16 +0700

    chore: fix build failed

--- packages/language/src/index.ts
+++ packages/language/src/index.ts
@@ -12,6 +12,27 @@ export { Tokenizer } from "./tokenizer";
 export { Parser } from "./parser";
 
 if (typeof window !== "undefined") {
+  customElements.define(
+    "vi-script",
+    class extends HTMLElement {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        setTimeout(() => {
+          const script = this.childNodes.item(0).textContent?.trim();
+
+          this.innerHTML = "";
+
+          if (script) {
+            executor.execute(script);
+          }
+        });
+      }
+    },
+  );
+
   // @ts-ignore
   window.VietScript = { parser, executor };
 }
