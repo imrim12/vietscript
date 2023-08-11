f56627ee08ea197afb7bab8fdb40a51e6773fd86
Nguyen Huu Nguyen Y <y.nguyen@paradox.ai>
2023-08-11 22:39:28 +0700

    chore: clean up identifier code

--- packages/language/src/nodes/statements/ContinueStatement.ts
+++ packages/language/src/nodes/statements/ContinueStatement.ts
@@ -1,5 +1,7 @@
 import { Parser } from "@lang/parser";
 
+import { Identifier } from "../identifier/Identifier";
+
 export class ContinueStatement {
   type = "ContinueStatement";
 
@@ -11,7 +13,7 @@ export class ContinueStatement {
     let label: ContinueStatement["label"] = null;
 
     if (parser.lookahead?.type === "Identifier") {
-      label = String(parser.eat("Identifier").value);
+      label = new Identifier(parser).name;
     }
 
     this.label = label;
