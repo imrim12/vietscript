028719c96287e68f7051619fc29b711690e7552b
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-04-06 00:20:18 +0700

    fix: import default issue with from token

--- packages/parser/src/nodes/declarations/__test__/declaration-import.test.ts
+++ packages/parser/src/nodes/declarations/__test__/declaration-import.test.ts
@@ -5,58 +5,60 @@ import { ImportDeclaration } from "../import/ImportDeclaration";
 
 describe("declaration-import.test", () => {
   it("should parse the syntax normally", () => {
-    const result = parser.parse(`sử dụng "./test-path"`, Declaration);
+    const result = parser.parse(`sử dụng "./test-path-1"`, Declaration);
 
     expect(toPlainObject(result)).toStrictEqual({
       type: "ImportDeclaration",
       specifiers: [],
       source: {
         type: "StringLiteral",
-        value: "./test-path",
+        value: "./test-path-1",
         extra: {
-          rawValue: "./test-path",
-          raw: '"./test-path"',
+          rawValue: "./test-path-1",
+          raw: '"./test-path-1"',
         },
         start: 8,
-        end: 21,
+        end: 23,
       },
       assertions: [],
     } as ImportDeclaration);
   });
 
   it("should parse the syntax normally", () => {
-    const result = parser.parse(`sử dụng * như là abc từ "./test-path"`, Declaration);
+    const result = parser.parse(`sử dụng * như là abc từ "./test-path-2"`, Declaration);
 
     expect(toPlainObject(result)).toStrictEqual({
       type: "ImportDeclaration",
+      importType: "value",
       specifiers: [
         {
           type: "ImportNamespaceSpecifier",
           local: {
             type: "Identifier",
-            name: "abc",
+            name: "_abc",
           },
         },
       ],
       source: {
         type: "StringLiteral",
-        value: "./test-path",
+        value: "./test-path-2",
         extra: {
-          rawValue: "./test-path",
-          raw: '"./test-path"',
+          rawValue: "./test-path-2",
+          raw: '"./test-path-2"',
         },
         start: 24,
-        end: 37,
+        end: 39,
       },
       assertions: [],
     } as ImportDeclaration);
   });
 
   it("should parse the syntax normally", () => {
-    const result = parser.parse(`sử dụng cái gì đó từ "./test-path"`, Declaration);
+    const result = parser.parse(`sử dụng cái gì đó từ "./test-path-3"`, Declaration);
 
     expect(toPlainObject(result)).toStrictEqual({
       type: "ImportDeclaration",
+      importType: "value",
       specifiers: [
         {
           type: "ImportDefaultSpecifier",
@@ -68,24 +70,25 @@ describe("declaration-import.test", () => {
       ],
       source: {
         type: "StringLiteral",
-        value: "./test-path",
+        value: "./test-path-3",
         extra: {
-          rawValue: "./test-path",
-          raw: '"./test-path"',
+          rawValue: "./test-path-3",
+          raw: '"./test-path-3"',
         },
-        start: 20,
-        end: 33,
+        start: 21,
+        end: 36,
       },
       assertions: [],
     } as ImportDeclaration);
   });
 
   it("should parse the syntax normally", () => {
-    const result = parser.parse(`sử dụng cái gì đó, { con cún : con chó con } từ "./test-path"`, Declaration);
+    const result = parser.parse(`sử dụng cái gì đó, { con cún : con chó con } từ "./test-path-4"`, Declaration);
     expect(toPlainObject(result)).toStrictEqual({
       type: "ImportDeclaration",
       specifiers: [
         {
+          type: "ImportDefaultSpecifier",
           local: {
             type: "Identifier",
             name: "_c225i_g236_273243",
@@ -105,13 +108,13 @@ describe("declaration-import.test", () => {
       ],
       source: {
         type: "StringLiteral",
-        value: "./test-path",
+        value: "./test-path-4",
         extra: {
-          rawValue: "./test-path",
-          raw: '"./test-path"',
+          rawValue: "./test-path-4",
+          raw: '"./test-path-4"',
         },
         start: 48,
-        end: 61,
+        end: 63,
       },
       assertions: [],
       importType: "value",
--- packages/parser/src/nodes/declarations/import/ImportNamespaceSpecifier.ts
+++ packages/parser/src/nodes/declarations/import/ImportNamespaceSpecifier.ts
@@ -3,7 +3,7 @@ import { Parser } from "@parser/parser";
 import { Keyword } from "@vietscript/shared";
 
 export class ImportNamespaceSpecifier {
-  type: "ImportNamespaceSpecifier";
+  type = "ImportNamespaceSpecifier";
 
   local: Identifier;
 
@@ -11,6 +11,16 @@ export class ImportNamespaceSpecifier {
     parser.eat("*");
 
     parser.eat(Keyword.AS);
+
+    if (typeof parser.lookahead?.value === "string") {
+      const fromToken = /\s+từ$/g.exec(parser.lookahead.value)?.[0];
+
+      if (fromToken) {
+        parser.tokenizer.rollback(fromToken.length);
+
+        parser.lookahead.value = parser.lookahead.value.replace(/\s+từ$/g, "");
+      }
+    }
 
     this.local = new Identifier(parser);
   }
